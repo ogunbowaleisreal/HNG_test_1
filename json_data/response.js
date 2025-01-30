2c8f8b067ad81849c44bfa4146c2cf597c14ce71
ogunbowaleisreal <ogunbowaleisreal4@gmail.com>
2025-01-30 19:49:50 +0100

    Update response.js

--- json_data/response.js
+++ json_data/response.js
@@ -1,3 +1,7 @@
-const response_data = {"email": "ogunbowaleisreal4@gmail.com", "current_datetime": new Date().toISOString(), "github_url": "https://github.com/ogunbowaleisreal/HNG_test_1.git"}
+const date_time = ()=>{
+    return new Date().toISOString()
+}
+
+const response_data = {"email": "ogunbowaleisreal4@gmail.com", "current_datetime": date_time() , "github_url": "https://github.com/ogunbowaleisreal/HNG_test_1.git"}
 
 module.exports = response_data 
