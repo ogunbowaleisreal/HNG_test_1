c8d9ea32da606920e9b09841360c912b47b818e6
ogunbowaleisreal <ogunbowaleisreal4@gmail.com>
2025-01-30 13:41:07 +0100

    first commit
    added all basic functionalities

---
+++ json_data/response.js
@@ -0,0 +1,4 @@
+const response_data = [{"email": "ogunbowaleisreal4@gmail.com", "current_datetime":new Date().toISOString(), "github_url": ""}]
+
+
+module.exports = response_data 
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3500;
+const response = require('./json_data/response')
+const cors = require('cors');
+
+app.use(cors())
+
+app.get('/', (req,res)=>{
+    res.status(200).send(response)
+})
+
+
+
+app.listen(PORT, ()=>{ console.log(`server running on port ${PORT}`)})
